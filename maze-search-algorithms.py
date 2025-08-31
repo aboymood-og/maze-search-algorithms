8b662b2bbe29cd015000dbe4440942e116737c55
aboymood-og <a.olmedocosta@gmail.com>
2025-08-31 03:57:00 -0400

    Refactor, comments, and implement terminal menu
    - Refactored the entire code for better readability  
    - Added comments for clarity and practicality  
    - Implemented a terminal menu to choose between BFS and DFS at runtime

--- maze-search-algorithms.py
+++ maze-search-algorithms.py
@@ -1,71 +1,111 @@
-#ALGORITMO FUNCIONAL, NO OPTIMO, PERO FUNCIONAL DE DFS Y BFS, MAÑANA REFACTORIZAR Y ARREGLAR ERRORES PARA QUE FUNCIONE Y SE ENTIENDA CORRECTAMENTE EL FUNCIONAMIENTO DEL MISMO
-maze = [["X"," "," "," ","X","X","X","X"],
-        ["X"," ","X"," "," "," ","X","X"],
-        ["X","s","X"," ","X"," "," "," "],
-        ["X","X","X"," ","X","X","g","X"]]
-
+#--------------------------- FUNCIONES ---------------------------#
+#funcion para encontrar "s" y "g" en el laberinto
 def find_start_goal(maze):
-  for y in range(len(maze)):
-    for x in range(len(maze[y])): 
-      if maze[y][x] == "s":
-        start = (y,x)
-      elif maze[y][x] == "g":
-        goal = (y,x)
+  start = None  #se inicializa cada variable a buscar
+  goal = None
+  for col in range(len(maze)): #Se recorre todo el laberinto buscando "s" y "g"
+    for row in range(len(maze[col])): 
+      if maze[col][row] == "s":
+        start = (col,row)
+      elif maze[col][row] == "g":
+        goal = (col,row)
+      if start != None and goal != None: #verifica dentro de cada iteración si se encontro "s" y "g"
         return start, goal
-
+  if start == None or goal == None: #si no se encontro "s" y "g" se usa esta funcion que devuelve un error y termina todo el programa
+    return start, goal
+    
+#funcion para encontrar posibles fronters, es decir, que no salgan del limite del laberinto, que no sean paredes/limites, y que no hallan sido recorridas antes
 def find_frontiers(agent):
-  
+  #se crea una lista que añade todas las posibles fronteras, es decir, los 4 lados que limitan la posición actual del agente
   posible_frontiers = [(agent[0], agent[1] - 1),     #izquierda
                        (agent[0] - 1, agent[1]),     #arriba
                        (agent[0], agent[1] + 1),     #derecha
                        (agent[0] + 1, agent[1])]     #abajo
-
+  #bucle para verificar que las posibles fronteras cumplan ciertas condiciones, de lo contrario se eliminan de la lista "posible_frontiers"
   i = 0 
-  while i != len(posible_frontiers):
-    if posible_frontiers is []:
-      break
-    elif posible_frontiers[i][0] >= len(maze) or posible_frontiers[i][1] >= len(maze[0]):
+  while i != len(posible_frontiers): 
+    if posible_frontiers is []: #verifica si hay posibles frontiers, en caso de que no y la lista este vacia, retorna la lista vacia 
+      return posible_frontiers
+    elif posible_frontiers[i][0] >= len(maze) or posible_frontiers[i][1] >= len(maze[0]): #verifica que la posible frontera no salga del margen del laberinto, si sale se elimina de "posible_frontiers"
       posible_frontiers.pop(i) 
-    elif (posible_frontiers[i][0] or posible_frontiers[i][1]) < 0:
+    elif posible_frontiers[i][0]  < 0 or posible_frontiers[i][1] < 0: #verifica que la posible frontera no sea negativa, si es negativa la elimina de "posible_frontiers"
       posible_frontiers.pop(i)
-    elif maze[posible_frontiers[i][0]][posible_frontiers[i][1]] == "X":
+    elif maze[posible_frontiers[i][0]][posible_frontiers[i][1]] == "X": #verifica que la posible frontera no sea una pared/limite, si es una pared o limite la elmina de "posible_frontiers"
       posible_frontiers.pop(i)
-    elif posible_frontiers[i] in path:
+    elif posible_frontiers[i] in explored_set: #verifica que la posible frontera no halla sido recorrido previamente por el agente, si lo fué la elimina de "posible_frontiers"
       posible_frontiers.pop(i)
-    else:
+    else: #en caso de que no cumpla ninguna condicion anterior, es valida para ser frontera por lo que se mantiene y aumenta la iteración para seguir con el siguiente elemento de la lista "posible_frontiers"
       i += 1
-  return posible_frontiers
+  return posible_frontiers #una vez se verifican las 4 posibles fronteras y no tenemos lista vacia, retorna la lista con las fronteras definitivas para el agente
 
-start, goal = find_start_goal(maze)
-print("Punto de inicio:", start, "\nPunto de meta: \t", goal)
+#funcion para imprimir el laberinto en pantalla
+def print_maze(maze):
+  for fila in maze:
+    print(' '.join(fila))
 
-#Empieza con una frontier que contiene el estado inicial
-frontier = []
-frontier.append(start)  
-print("Frontier:", frontier)
+#funcion para elegir algoritmo de busqueda
+def choose_algorithm():    
+  while True:
+    aux = input("Eliga que algoritmo usar: \n (1): BFS (cola) \n (2): DFS (pila)\n\nEscriba 1 o 2:")
+    if aux == "1":
+      print("\nSolución con BFS:")
+      alg = 0
+      return alg
+    elif aux == "2":
+      print("\nSolución con DFS:")
+      alg = -1
+      return alg
+    else:
+      print("Debe elegir entre [1,2]. Intentar denuevo.\n")
+#--------------------------- FUNCIONES ---------------------------# 
 
-#Repetir
-path = []
-agent = None
-cont = 0
-while agent != goal:
-  print("\nIteración ", cont)
-  cont +=1 
-  path.append(agent)
-  print("Path: ", path)
-  #Si la frontera es vacía, entonces no hay solución
-  if len(frontier) == 0:
-    print("No hay solución")
-    break
-  #Borrar un nodo de la frontera
-  agent = frontier.pop(0)
-  print("Agente:", agent)
+#---------------------- PROGRAMA PRINCIPAL -----------------------# 
+#inicializar laberinto
+maze = [["X"," "," "," ","X","X","X","X"],  # X = paredes/limites3
+        ["X"," ","X"," "," "," ","X","X"],  # s = punto de inicio
+        ["X","s","X"," ","X"," "," "," "],  # g = punto de meta
+        ["X","X","X"," ","X","X","g","X"]]
+
+start, goal = find_start_goal(maze) #busca "s" y "g" en el laberinto usando la funcion previamente descrita y los guarda en variables con dichos nombres
+print("El laberinto a buscar una salida:\n") #imprime por pantalla un mensaje de inicio
+print_maze(maze) #dibuja el laberinto en terminal
+print("") #solo un salto de linea xd
+
+if start != None and goal != None: #verifica que luego de ejecutar el "find_start_goal" se hayan obtenido dichos valores y no queden vacios
+
+  print("Punto de inicio:", start, "\nPunto de meta: \t", goal, "\n") #imprime por pantalla el "s" y "g" identificado usando find_start_goal 
+  alg = choose_algorithm() #ejecuta funcion para elegir si usar dfs o bfs
+
+  #ALGORITMO DE BUSQUEDA 
+  frontier = [] #se inicializa la frontera 
+  frontier.append(start) #se agrega a la frontera el punto de partida ya que es como si el algoritmo comenzara desde un agente que solo tiene esta frontera
+  explored_set = [] #se inicializa una lista que almacena las coordenadas por las cuales el agente ya pasó
+  agent = None #se inicializa el agente, el cual como comienza fuera del laberinto y solo tiene de frontera al punto de inicio queda como None
+  cont = 0 #contador arbitrario para el while y saber cuantas iteraciones se han echo
+  while agent != goal: #el while tiene como condición que el agente no este en la meta, si esta en la meta sale del while
+    print("\nIteración ", cont)
+    cont +=1 
+    if agent != None: #verifica que el agente no sea None, es decir, que aun el algoritmo no empiece como tal, si este es diferente de none lo agregará al explored set.
+      explored_set.append(agent)
+    print("explored_set: ", explored_set)
+    #Si la frontera es vacía, entonces no hay solución
+    if len(frontier) == 0: #verifica si el tamaño de la frontera en algun momento es 0, si es 0 el laberinto no tiene solución.
+      print("\nNo hay solución para este laberinto.")
+      break
+    agent = frontier.pop(alg) #aca borra un nodo de la frontera en base a la decisión tomada, si se usa bfs toma 0 por lo que es una cola, si usa dfs toma -1 por lo que usa pila.
+    print("Agente:", agent)
+     
+    frontier += find_frontiers(agent) #agrega las fronteras encontradas para el agente usando "find_frontiers" a la lista de fronteras totales, actuando como pila o cola dependiendo.
+    print("frontier: ", frontier)
 
   if agent == goal:
     print("\n¡Meta alcanzada!")
-    print("Camino recorrido: ", path)
-    break
-    
-  #Expandir nodo, agregar nodos resultantes a la frontera
-  frontier += find_frontiers(agent)
-  print("frontier: ", frontier)
+    print("Laberinto con la solución: ") #me falta programar esto pero para ello necesio algo mas importante que es la siguiente lines
+    print("Solución lograda con el algoritmo elegído: ") #me falta implementar esto, de tal forma que en alguna lista supongo se almacene el camino correcto para llegar de "s" a "g", ya que de momento solo me esta guardando el explored set pero esto no es una solución como tal.
+    print("Nodos explorados para llegar a la solución: ", explored_set)
+      
+elif start == None or goal == None: #verifica que luego de ejecutar el "find_start_goal" se hayan obtenido none, si es así imprime un error por pantalla y termina el programa
+  print("El laberinto debe contener 's' (inicio) y 'g' (meta).")
+
+else: #si ninguna de las 2 condiciones anteriores se cumple (raro), imprime otro mensaje de error y se termina el programa
+  print("Algo ocurrio...")
